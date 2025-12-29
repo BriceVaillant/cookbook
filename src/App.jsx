5738e2510133c725f4bd9d8cac34df6eccbce785
BriceVaillant <naw.brice@gmail.com>
2025-12-29 10:04:18 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { Routes, Route } from 'react-router-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
+import { UserProvider } from './contexts/UserContext';
+import { useLocation } from 'react-router-dom';
+
+import Navbar from './Navbar';
+import RecipeHome from './RecipeHome';
+import RecipeList from './RecipeList';
+
+import './index.css';
+import './Navbar.css';
+import './RecipeHome.css';
+import './RecipeList.css';
+import './RecipeDetails.css';
+import './RecipeEdit.css';
+import './RecipeAdd.css';
+
+
+function RecipeApp() {
+
+  const location = useLocation();
+
+  return (
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: `${window.location.origin}`
+      }}
+      cacheLocation="localstorage"
+      useRefreshTokens={true}
+    >
+      <UserProvider>
+        <div id="RecipeApp">
+          <>
+            <Navbar />
+            <Routes location={location} key={location.pathname}>
+              <Route index element={<RecipeHome />} />
+              <Route path="/home" element={<RecipeHome />} />
+              <Route path="/recipes" element={<RecipeList />} />
+            </Routes>
+          </>
+        </div>
+      </UserProvider>
+    </Auth0Provider>
+
+  );
+}
+
+export default RecipeApp
